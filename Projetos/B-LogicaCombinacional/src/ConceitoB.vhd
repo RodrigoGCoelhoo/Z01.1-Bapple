1f944da76506d3b11475c7963c4197eb5ff42eb8
borg <fabricio.neri.ismart@gmail.com>
2021-03-22 12:50:33 -0300

    configurando o scrum do projeto

--- Projetos/B-LogicaCombinacional/src/ConceitoB.vhd
+++ Projetos/B-LogicaCombinacional/src/ConceitoB.vhd
@@ -15,7 +15,8 @@ entity ConceitoB is
 		SW       : in  std_logic_vector(9 downto 0);
         HEX0     : out std_logic_vector(6 downto 0); -- 7seg0
         HEX1     : out std_logic_vector(6 downto 0); -- 7seg0
-        HEX2     : out std_logic_vector(6 downto 0); -- 7seg0
+		HEX2     : out std_logic_vector(6 downto 0); -- 7seg0
+
 		LEDR     : out std_logic_vector(9 downto 0)
 	);
 end entity;
@@ -24,6 +25,7 @@ end entity;
 -- Implementacao do bloco --
 ----------------------------
 architecture rtl of ConceitoB is
+
 
 --------------
 -- signals
@@ -34,5 +36,41 @@ architecture rtl of ConceitoB is
 ---------------
 begin
 
+
+	with SW(3 downto 0) select
+		HEX0 <= "0000001" when "0000", --numero 0 
+				"1001111" when "0001", --numero 1
+				"0010010" when "0010", --numero 2
+				"0000110" when "0011", --numero 3
+				"1001100" when "0100", --numero 4
+				"0100100" when "0101", --numero 5
+				"0100000" when "0110", --numero 6
+				"0001111" when "0111", --numero 7
+				"1111111" when "1000", --numero 8
+				"0000100" when others; --numero 9
+	
+	with SW(6 downto 3) select
+		HEX1 <= "0000001" when "0000", --numero 0 
+				"1001111" when "0001", --numero 1
+				"0010010" when "0010", --numero 2
+				"0000110" when "0011", --numero 3
+				"1001100" when "0100", --numero 4
+				"0100100" when "0101", --numero 5
+				"0100000" when "0110", --numero 6
+				"0001111" when "0111", --numero 7
+				"1111111" when "1000", --numero 8
+				"0000100" when others; --numero 9
+
+	with SW(9 downto 6) select
+		HEX2 <= "0000001" when "0000", --numero 0 
+				"1001111" when "0001", --numero 1
+				"0010010" when "0010", --numero 2
+				"0000110" when "0011", --numero 3
+				"1001100" when "0100", --numero 4
+				"0100100" when "0101", --numero 5
+				"0100000" when "0110", --numero 6
+				"0001111" when "0111", --numero 7
+				"1111111" when "1000", --numero 8
+				"0000100" when others; --numero 9
 
 end rtl;
--- Projetos/B-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/B-LogicaCombinacional/src/TopLevel.vhd
@@ -19,9 +19,18 @@ use work.all;
 ----------------------------
 entity TopLevel is
     port(
-        SW      : in  std_logic_vector(9 downto 0);
-        HEX0    : out std_logic_vector(6 downto 0); -- 7seg0
-        LEDR    : out std_logic_vector(9 downto 0)
+      CLOCK_50 : in  std_logic;
+		SW       : in  std_logic_vector(9 downto 0);
+      HEX0     : out std_logic_vector(6 downto 0); -- 7seg0
+      HEX1     : out std_logic_vector(6 downto 0); -- 7seg0
+		HEX2     : out std_logic_vector(6 downto 0); -- 7seg0
+		
+		intermediario1 		 : inout std_logic_vector(3 downto 0);
+		intermediario2		 	 : inout std_logic_vector(3 downto 0);
+		intermediario3 		 : inout std_logic_vector(3 downto 0);
+
+
+		LEDR     : out std_logic_vector(9 downto 0)
     );
 end entity;
 
@@ -38,7 +47,41 @@ architecture rtl of TopLevel is
 -- implementacao
 ---------------
 begin
+	
 
-	HEX0 <= "0100100";
-  
+	with SW(3 downto 0) select
+		HEX0 <= "0000001" when "0000", --numero 0 
+				"1001111" when "0001", --numero 1
+				"0010010" when "0010", --numero 2
+				"0000110" when "0011", --numero 3
+				"1001100" when "0100", --numero 4
+				"0100100" when "0101", --numero 5
+				"0100000" when "0110", --numero 6
+				"0001111" when "0111", --numero 7
+				"1111111" when "1000", --numero 8
+				"0000100" when others; --numero 9
+	
+	with SW(6 downto 3) select
+		HEX1 <= "0000001" when "0000", --numero 0 
+				"1001111" when "0001", --numero 1
+				"0010010" when "0010", --numero 2
+				"0000110" when "0011", --numero 3
+				"1001100" when "0100", --numero 4
+				"0100100" when "0101", --numero 5
+				"0100000" when "0110", --numero 6
+				"0001111" when "0111", --numero 7
+				"1111111" when "1000", --numero 8
+				"0000100" when others; --numero 9
+
+	with SW(9 downto 6) select
+		HEX2 <= "0000001" when "0000", --numero 0 
+				"1001111" when "0001", --numero 1
+				"0010010" when "0010", --numero 2
+				"0000110" when "0011", --numero 3
+				"1001100" when "0100", --numero 4
+				"0100100" when "0101", --numero 5
+				"0100000" when "0110", --numero 6
+				"0001111" when "0111", --numero 7
+				"1111111" when "1000", --numero 8
+				"0000100" when others; --numero 9
 end rtl;
