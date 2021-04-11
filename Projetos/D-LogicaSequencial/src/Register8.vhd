7d7139f5e01122318aacf990dca662b1d1b5a6e8
borg <fabricio.neri.ismart@gmail.com>
2021-04-11 08:56:10 -0300

    subindo o register 8

--- Projetos/D-LogicaSequencial/src/Register8.vhd
+++ Projetos/D-LogicaSequencial/src/Register8.vhd
@@ -25,7 +25,26 @@ architecture arch of Register8 is
 		);
 	end component;
 
+
+	SIGNAL binarydigitout0, binarydigitout2, binarydigitout3, binarydigitout4, binarydigitout5, binarydigitout6, binarydigitout7, binarydigitout1: std_logic;
 begin
 
+	bin0: BinaryDigit Port Map (clock, input(0), load, binarydigitout0);
+	bin1: BinaryDigit Port Map (clock, input(1), load, binarydigitout1);
+	bin2: BinaryDigit Port Map (clock, input(2), load, binarydigitout2);
+	bin3: BinaryDigit Port Map (clock, input(3), load, binarydigitout3);
+	bin4: BinaryDigit Port Map (clock, input(4), load, binarydigitout4);
+	bin5: BinaryDigit Port Map (clock, input(5), load, binarydigitout5);
+	bin6: BinaryDigit Port Map (clock, input(6), load, binarydigitout6);
+	bin7: BinaryDigit Port Map (clock, input(7), load, binarydigitout7);
+
+	output(0) <= binarydigitout0;
+	output(1) <= binarydigitout1;
+	output(2) <= binarydigitout2;
+	output(3) <= binarydigitout3;
+	output(4) <= binarydigitout4;
+	output(5) <= binarydigitout5;
+	output(6) <= binarydigitout6;
+	output(7) <= binarydigitout7;
 
 end architecture;
